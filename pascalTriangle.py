f06e9bc2536b87ee837bfd079190043a2963b254
ashok <ashokjain001@gmail.com>
2018-05-22 23:30:29 -0500

    Adding spaces to make it look pretty

--- pascalTriangle.py
+++ pascalTriangle.py
@@ -4,7 +4,12 @@ def pascalTriangle(n):
 	for i in range(n):
 		val=''
 		for j in range(i+1):
-			val+=str(factorial(i)/(factorial(j)*factorial(abs(i-j))))
-		print val
+			val+=' '+str(factorial(i)/(factorial(j)*factorial(abs(i-j))))+' '
+		print (' '*n)+val
+		n-=1
 
 print(pascalTriangle(10))
+
+'''
+Pascal Triangle has a special property of  
+'''
