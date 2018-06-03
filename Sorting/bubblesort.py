c05a00b6a26a60d8fae2570b291f5ca088635907
ashok <ashokjain001@gmail.com>
2018-06-02 20:31:19 -0500

    Adding output to the algorithm

--- Sorting/bubblesort.py
+++ Sorting/bubblesort.py
@@ -1,6 +1,8 @@
 '''Python implementation of Bubblesort
 Bubble sort has a run time of O(n^2)
-After each pass the largest element is pushed towards the end and the Iteration continues till all the elements are in correct position'''
+After each pass the largest element is pushed 
+towards the end and the Iteration continues 
+till all the elements are in correct position'''
 def bubblesort(array):
   l=len(array)
   for i in range(l-1,-1,-1):
@@ -11,3 +13,15 @@ def bubblesort(array):
   return array    
 
 print bubblesort([40,7,90,20,60,21,6, 19])
+
+''' Output
+[7, 40, 20, 60, 21, 6, 19, 90]
+[7, 20, 40, 21, 6, 19, 60, 90]
+[7, 20, 21, 6, 19, 40, 60, 90]
+[7, 20, 6, 19, 21, 40, 60, 90]
+[7, 6, 19, 20, 21, 40, 60, 90]
+[6, 7, 19, 20, 21, 40, 60, 90]
+[6, 7, 19, 20, 21, 40, 60, 90]
+[6, 7, 19, 20, 21, 40, 60, 90]
+[6, 7, 19, 20, 21, 40, 60, 90]
+'''
