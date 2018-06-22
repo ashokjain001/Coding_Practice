5078b1391e1a84457ab3c5ae5bad19e244d06ae8
ashok <ashokjain001@gmail.com>
2018-06-21 19:07:02 -0500

    refactor: updates to the code

--- problem_Solving/countOccurence.py
+++ problem_Solving/countOccurence.py
@@ -1,24 +1,65 @@
-def countOccurence(l,low,high,val, count):
-	print l[low:high+1]
+'''Given a sorted array arr[] and a number x, 
+write a function that counts the occurrences of x in arr[]. 
+Expected time complexity is O(Logn)'''
 
-	mid=(low+high)/2
 
-	print low, mid, high
-	
-	if len(l[low:high])<2:
-		count=+1
-		return
+def firstOccurence(l,low,high,val):
 
-	if l[mid]==val:
+  start=low
 
-		countOccurence(l,low,mid,val, count)
-		countOccurence(l,mid,high,val, count)
+  if len(l[low:high])<2:
+  	print 'length is less than 2', l[low:high], start
+  	return start
 
-	if l[mid]<val:
-		countOccurence(l,mid+1,high,val, count)
+  mid=int((low+high)/2)
 
-	if l[mid]>val:
-		countOccurence(l,low,mid-1,val, count)
-	return count
+  print(low, mid, high, l[low:mid])
 
-print countOccurence([0,1,1,3,3,3,3,6,8,10,11,11],0,11,3,0)
+  if l[mid]==val:
+  	return firstOccurence(l,low,mid,val)
+ 
+  if l[mid]<val:
+  	print l[mid],' is less than ',val
+  	return firstOccurence(l,mid+1,high,val)
+
+  if l[mid]>val:
+  	print l[mid],' is greater than ',val
+  	return firstOccurence(l,low,mid-1,val)
+  
+  return start
+
+def lastOccurence(l,low,high,val):
+
+  end=low
+
+  if len(l[low:high])<2:
+  	print('length is less than 2', l[low:high])
+  	return end
+
+  mid=int((low+high)/2)
+
+  print(low, mid, high, l[low:mid])
+
+  if l[mid]==val:
+  	return lastOccurence(l,mid,high,val)
+ 
+  if l[mid]<val:
+  	print(l[mid],' is less than ',val)
+  	return lastOccurence(l,mid+1,high,val)
+
+  if l[mid]>val:
+  	print(l[mid],' is greater than ',val)
+  	return lastOccurence(l,low,mid-1,val)
+  
+  return end	
+
+def countOccurences(l,val):
+	low=0
+	high=len(l)-1
+
+	first=firstOccurence(l,low,high,val)
+	end=lastOccurence(l,low,high,val)
+
+	return end-first+1
+
+print(countOccurences([0,1,1,3,3,3,3,6,8,10,11,11],3))
