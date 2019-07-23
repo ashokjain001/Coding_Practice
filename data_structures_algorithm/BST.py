fd19d0950b10c83b83819ab302ba41df92307f99
ashok <ashokjain001@gmail.com>
2019-07-22 20:58:52 -0500

    Binary Tree Traversal

--- data_structures_algorithm/BST.py
+++ data_structures_algorithm/BST.py
@@ -8,22 +8,24 @@ class BST(object):
 	def __init__(self,root= None):
 		self.root = root #pointer to root node and setting the tree as empty
 
-	def insert(self, new_element=None):
-		self.insert_(self.root, new_element)
 
-	def insert_(self, root=None, new_element=None):
+	def insert(self, value):
+		self.insert_(self.root, value)
+
+	def insert_(self, root, value):
 		
 		if root == None:
-			root = new_element
+			root = Element(value)
 			return root   # returns the address of the root
 
-		if root.value >= new_element.value:
-			root.left = self.insert_(root.left ,new_element)
+		if root.value >= value:
+			root.left = self.insert_(root.left ,value)
 
-		if root.value < new_element.value:
-			root.right = self.insert_(root.right ,new_element)
+		if root.value < value:
+			root.right = self.insert_(root.right ,value)
 
 		return root
+
 
 	def search(self, value):
 		return self.search_(root=self.root, value=value)
@@ -42,39 +44,114 @@ class BST(object):
 		if root.value < value:
 			return self.search_(root.right,value)
 
+
 	def printTree(self):
 		if root != None:
 			self.printTree_(self.root)
 
 	def printTree_(self,root=None):
 		if root != None:
-			self.printTree_(root.left)
 			print root.value
+			self.printTree_(root.left)
+			
 			self.printTree_(root.right)
 
 
+	def minTree(self):
 
+		current=self.root
+
+		if current == None:
+			return 'No nodes in the Binary Search tree'
+
+		while current.left!=None:
+			current = current.left
+		return current.value
+
+	def maxTree(self):
+		
+		current = self.root
+		if current == None:
+			return 'No nodes in the Binary Search tree'
+
+		while current.right!=None:
+			current = current.right
+		return current.value	
+
+
+	def findheight(self):
+		return self.findheight_(self.root)
+
+	def findheight_(self, root):
+
+		if root == None:
+			return -1 
+
+		leftHeight = self.findheight_(root.left)
+		rightHeight = self.findheight_(root.right)
+
+		return max(leftHeight,rightHeight)+1
+
+
+	def preorder_Traversal(self):
+		return self.preorder_Traversal_(self.root)
+
+	def preorder_Traversal_(self,root):
+		
+		if root!=None:
+			print root.value
+			self.preorder_Traversal_(root.left)
+			self.preorder_Traversal_(root.right)
+
+
+	def inorder_Traversal(self):
+		return self.inorder_Traversal_(self.root)
+
+	def inorder_Traversal_(self,root):
+
+		if root!=None:
+			self.inorder_Traversal_(root.left)
+			print root.value
+			self.inorder_Traversal_(root.right)
+
+
+	def postorder_Traversal(self):
+		return self.postorder_Traversal_(self.root)
+
+	def postorder_Traversal_(self,root):
+
+		if root!=None:
+			self.postorder_Traversal_(root.left)
+			self.postorder_Traversal_(root.right)
+			print root.value
 
 e1 = Element(15)
-e2 = Element(10)
-e3 = Element(20)
-e4 = Element(8)
-e5 = Element(9)
-e6 = Element(19)
-e7 = Element(21)
-e8 = Element(25)
-
 root = BST(e1)
 
-root.insert(new_element=e2)
-root.insert(new_element=e3)
-root.insert(new_element=e4)
-root.insert(new_element=e5)
-root.insert(new_element=e6)
-root.insert(new_element=e7)
-root.insert(new_element=e8)
-#print root.search(21)
+root.insert(10)
+root.insert(20)
+root.insert(8)
+root.insert(9)
+root.insert(19)
+root.insert(21)
+root.insert(25)
+root.insert(5)
+
 
 root.printTree()
+
+print 'Minimum value node in the Tree is', root.minTree()
+
+print 'Maximum value node in the Tree is' ,root.maxTree()
+
+print 'Height of the Tree is', root.findheight()
+
+print 'preorder Traversal of the Tree', root.preorder_Traversal()
+
+print 'Inorder Traversal of the Tree', root.inorder_Traversal()
+
+print 'postorder Traversal of the Tree', root.postorder_Traversal()
+#print root.search(21)
+
 
 
