cc880bc6e9869cbb894265644170aaf65ff6373f
ashok <ashokjain001@gmail.com>
2019-07-31 23:44:56 -0500

    Update to Bubble sort

--- Sorting/bubblesort.py
+++ Sorting/bubblesort.py
@@ -4,13 +4,12 @@ After each pass the largest element is pushed
 towards the end and the Iteration continues 
 till all the elements are in correct position'''
 def bubblesort(array):
-  l=len(array)
-  for i in range(l-1,-1,-1):
-    for i in range(l-1):
-      if array[i]>array[i+1]:
-        array[i],array[i+1]=array[i+1],array[i]
-    print array
-  return array    
+	l=len(array)
+	for i in range(l-1):
+		for j in range(l-1):
+			if array[j]>array[j+1]:
+				array[j],array[j+1]=array[j+1],array[j]
+	return array
 
 print bubblesort([40,7,90,20,60,21,6, 19])
 
